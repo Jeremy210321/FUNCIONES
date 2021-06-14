119a1fd9f890d660d2c6c9eadede1c7212797221
Jeremy210321 <jeremy.leon@epn.edu.ec>
2021-06-14 09:51:44 -0500

    Update FUNCIONES.cpp
    Agregando cambios

--- ejercicio/FUNCIONES.cpp
+++ ejercicio/FUNCIONES.cpp
@@ -130,6 +130,33 @@ long int potencia(int c, int x)
   }
   return resultado;
 }
+void reinas(int k, vector<int> col, vector<int> diag45, vector<int> diag135)
+{
+    if( k == NREINAS )
+    {
+	printf("%3d:", nro_sol++);
+        for(int j=0; j<NREINAS; j++)
+ cout << " (" << j+1 << "," << sol[j] << ")";
+        cout << endl;
+    }
+    else
+    {
+        for(int j=1; j<=NREINAS; j++)
+            if( !contiene(col, j) && !contiene(diag45, j-k) && !contiene(diag135, j+k) )
+            {
+                sol[k] = j;
+
+                col.push_back(j);
+                diag45.push_back(j-k);
+                diag135.push_back(j+k);
+
+                reinas(k+1,col, diag45, diag135);
+
+                col.pop_back();
+                diag45.pop_back();
+                diag135.pop_back();
+            }
+    }
 void EJERCICIO2()
 {
   int h, m, s, h_despues, m_despues, s_despues;
